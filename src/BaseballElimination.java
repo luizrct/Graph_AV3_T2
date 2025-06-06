9ef51e6b790d5004df125bb24566253b9bf0a784
luiz <luizchavestillio1@gmail.com>
2025-06-06 01:18:57 -0300

    comecei a construção do grafo

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -1,8 +1,10 @@
+import assets.FlowEdge;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class BaseballElimination {
-
+    private static int INFINITY = Integer.MAX_VALUE;
     private static int N;
     private int[] wins;
     private int[] losses;
@@ -18,6 +20,7 @@ public class BaseballElimination {
             losses = new int[N];
             remaining = new int[N][];
             teams = new String[N];
+            eliminado = new boolean[N];
             int c = 0;
             while (true){
                 linha = br.readLine();
@@ -29,6 +32,23 @@ public class BaseballElimination {
             }
         } catch (Exception erro){
             System.out.println("Erro na leitura do arquivo: "+erro);
+        }
+
+        //DEFININDO QUAIS TIMES FORAM ELIMINADOS
+        if(N == 1){
+            eliminado[0] = false;
+        }else if(N == 2){
+            eliminado[0] = false;
+            eliminado[1] = false;
+            //Comparação direta entre 2 times
+            if(wins[0] + remaining[0][0] < wins[1]){
+                eliminado[0] = true;
+            }else if(wins[1] + remaining[1][0] < wins[0]){
+                eliminado[1] = true;
+            }
+        }else{
+            int V = 2 + ((N - 1) * (N - 2))/ 2 + (N - 1);
+            verificacaoEliminacao(V, 0);
         }
     }
 
@@ -43,6 +63,10 @@ public class BaseballElimination {
             }
         }
         return null;
+    }
+
+    public String time(int i){
+        return teams[i];
     }
 
     public int wins(String team){
@@ -75,5 +99,65 @@ public class BaseballElimination {
         for(int i = 3; i < palavras.length; i++){
             remaining[i - 3] = Integer.valueOf(palavras[i]);
         }
+        this.remaining[c] = remaining;
+    }
+
+
+
+    private boolean verificacaoEliminacao(int V, int time){
+        FlowNetwork fn = new FlowNetwork(V);
+        int[] times = new int[N-1];
+        int c2 = 0;
+        for(int i = 0; i < N; i++){
+            if(i != time){
+                times[c2] = i;
+                c2++;
+            }
+        }
+        int n_partidas = ((N - 1) * (N - 2)) / 2;
+        int[][] partidas =  new int[n_partidas][];
+        int c = 0;
+        for(int i = 0; i < N; i ++){
+            if(i != time){
+                for(int j = 1; j < remaining[i].length; j++){
+                    if(j-1 != time && i != j-1){
+                        if(c == n_partidas){
+                            break;
+                        }
+                        if(verificarPartidas(partidas, new int[]{i, j-1}, c)){
+                            int[] partida = {i, j-1, remaining[i][j]};
+                            partidas[c] = partida;
+                            c++;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        for(int i = 1; i < n_partidas+1; i++){
+            //adicionando as arestas das partidas
+            fn.addEdge(new FlowEdge(0, i, partidas[i-1][2]));
+
+            //adicionando as arestas dos times
+            fn.addEdge(new FlowEdge(i, i+n_partidas, INFINITY));
+            fn.addEdge(new FlowEdge(i, i+n_partidas, INFINITY));
+        }
+
+
+        return false;
+    }
+
+
+    private boolean verificarPartidas(int[][] partidas, int[] partida, int c){
+        for(int i = 0; i < c; i++){
+            if(partidas[i][0] == partida[0] && partidas[i][1] == partida[1]){
+                return false;
+            }
+            if(partidas[i][0] == partida[1] && partidas[i][1] == partida[0]){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,21 @@ import java.io.BufferedWriter;
 
 public class Main {
     public static void main(String[] args) {
-        String[] arquivos = {"src/data/teams1.txt"};
-        BaseballElimination be = new BaseballElimination(arquivos[0]);
+        String[] arquivos = {"src/data/teams1.txt", "src/data/teams4.txt"};
+        BaseballElimination be = new BaseballElimination(arquivos[1]);
+        System.out.println("Número de times: "+be.numberOfTeams());
+        System.out.println("INFORMAÇÃO GERAL DOS TIMES: ");
 
+        for(int i = 0; i < be.numberOfTeams(); i++){
+            String nomeTime = be.time(i);
+            String eliminado = "";
+            if(be.isEliminated(nomeTime)){
+                eliminado = "sim";
+            }else{
+                eliminado = "não";
+            }
+            System.out.println("Time: "+i+"; Nome: "+nomeTime+"; Vitórias: "+be.wins(nomeTime)+"; Derrotas: "+be.losses(nomeTime)+"; Sobrando: "+be.remaining(nomeTime)+" Eliminado: "+eliminado);
+            System.out.println(" ");
+        }
     }
 }
